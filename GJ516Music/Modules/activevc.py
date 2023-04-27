29e4d536a55711aedb202e3650e73af3308b9af6
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-04-28 00:05:51 +0530

    Update activevc.py

--- GJ516Music/Modules/activevc.py
+++ GJ516Music/Modules/activevc.py
@@ -30,7 +30,7 @@ from GJ516Music.Helpers.inline import close_key
 
 @app.on_message(filters.command("activevc") & SUDOERS)
 async def activevc(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ʟɪsᴛ...")
+    mystic = await message.reply_text("𝙅𝙖𝙮 𝙂𝙚𝙩𝙩𝙞𝙣𝙜 𝘼𝙘𝙩𝙞𝙫𝙚 𝙑𝙤𝙞𝙘𝙚𝘾𝙝𝙖𝙩𝙨 𝙇𝙞𝙨𝙩... ☘")
     chats = await get_active_chats()
     text = ""
     j = 0
@@ -38,7 +38,7 @@ async def activevc(_, message: Message):
         try:
             title = (await app.get_chat(chat)).title
         except Exception:
-            title = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+            title = "𝙋𝙧𝙞𝙫𝙖𝙩𝙚 𝘾𝙝𝙖𝙩 "
         if (await app.get_chat(chat)).username:
             user = (await app.get_chat(chat)).username
             text += f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})\n"
@@ -46,10 +46,11 @@ async def activevc(_, message: Message):
             text += f"<b>{j + 1}. {title}</b> [`{chat}`]\n"
         j += 1
     if not text:
-        await mystic.edit_text("ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄʙᴏᴛ...")
+        await mystic.edit_text("𝙉𝙤 𝘼𝙘𝙩𝙞𝙫𝙚 𝙑𝙞𝙙𝙚𝙤 𝘾𝙝𝙖𝙩 𝙊𝙣 𝙈𝙪𝙨𝙞𝙘𝘽𝙤𝙩.... ❊
+")
     else:
         await mystic.edit_text(
-            f"**ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄ ʙᴏᴛ :**\n\n{text}",
+            f"**𝙇𝙞𝙨𝙩 𝙤𝙛 𝘾𝙪𝙧𝙧𝙚𝙣𝙩𝙡𝙮 𝘼𝙘𝙩𝙞𝙫𝙚 𝙑𝙤𝙞𝙘𝙚 𝙘𝙝𝙖𝙩 𝙊𝙣 𝙈𝙪𝙨𝙞𝙘 𝘽𝙤𝙩  :**\n\n{text}",
             reply_markup=close_key,
             disable_web_page_preview=True,
         )
