5d9fb8c9a09ea7bbb4f2efa4c7c5e21640886eb6
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-04-28 08:00:54 +0530

    Update __init__.py

--- GJ516Music/__init__.py
+++ GJ516Music/__init__.py
@@ -92,8 +92,8 @@ async def GJ516_startup():
     ASS_USERNAME = getme2.username
     ASS_MENTION = getme2.mention
     try:
-        await app2.join_chat("DevilsHeavenMF")
-        await app2.join_chat("GJ516Association")
+        await app2.join_chat("GJ516_DISCUSS_GROUP")
+        await app2.join_chat("GJ516_DISCUSS_GROUP")
     except:
         pass
 
