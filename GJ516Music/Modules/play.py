a4e7eb34405abafac50e1f594b9791c45b8ee4a6
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-06-05 18:59:55 +0530

    Update play.py

--- GJ516Music/Modules/play.py
+++ GJ516Music/Modules/play.py
@@ -178,7 +178,8 @@ async def play(_, message: Message):
         file_path = audio_dl(url)
     else:
         if len(message.command) < 2:
-            return await GJ516.edit_text(" 𝙂𝙞𝙫𝙚 𝙈𝙚 𝙔𝙤𝙪𝙧 𝙌𝙪𝙚𝙧𝙮 𝙒𝙝𝙞𝙘𝙝 𝙔𝙤𝙪 𝙬𝙖𝙣𝙩 𝙩𝙤 𝙋𝙡𝙖𝙮/n/n 𝙀𝙭𝙖𝙢𝙥𝙡𝙚: /play song name .... ")
+            return await GJ516.edit_text("𝙂𝙞𝙫𝙚 𝙈𝙚 𝙔𝙤𝙪𝙧 𝙌𝙪𝙚𝙧𝙮 𝙒𝙝𝙞𝙘𝙝 𝙔𝙤𝙪 𝙬𝙖𝙣𝙩 𝙩𝙤 𝙋𝙡𝙖𝙮 𝙀𝙭𝙖𝙢𝙥𝙡𝙚: /play song name .... ")
+
         await GJ516.edit_text("💸")
         query = message.text.split(None, 1)[1]
         try:
