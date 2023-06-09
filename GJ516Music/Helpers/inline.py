bde2ae4b7d995fc1c462c70b05927d167c2fc2be
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-06-09 12:57:08 +0530

    Update inline.py

--- GJ516Music/Helpers/inline.py
+++ GJ516Music/Helpers/inline.py
@@ -62,8 +62,6 @@ pm_buttons = [
             url=f"https://t.me/{BOT_USERNAME}?startgroup=true" ),
    
         InlineKeyboardButton(text="❰𝙃𝙀𝙇𝙋❱", callback_data="GJ516_help"),
-    ],[
-InlineKeyboardButton(text="❰𝙊𝙬𝙣𝙚𝙧❱",user_id=config.OWNER_ID),
     ],
 ]
 
