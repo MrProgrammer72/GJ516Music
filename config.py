1199e0c7a01c504522c032c570b9a8b1daa5e729
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-07-02 12:50:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/eb6058c1ae3cb90f517e5.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/db62e20be4e685a5d0716.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/3d3af9872f3a949391486.jpg")
 
 SESSION = getenv("SESSION", None)
